ac8460679a51d3be4d7a6e63a111a128b47ec765
Hani Saravanan <hanisaravanan@gmail.com>
2024-05-24 17:34:56 -0400

    footer

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@/assets/styles/globals.css';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
+
+export const metadata = {
+    title : 'Rentify | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rentals, find rentals, find properties'
+}
+
+const MainLayout = ({children}) => {
+  return (
+    <html lang = 'en'>
+        <body>
+        <Navbar/>
+            <main>{children}</main>
+            <Footer/>
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout;
